11799213aa33d324db0d035536349afa229b9f2e
KaramPreet81 <karam_pss@cs.iitr.ac.in>
2023-08-20 23:08:38 +0530

    adjList and adjmatrix is added

--- single.h
+++ single.h
@@ -96,3 +96,138 @@
     }
     
   };
+
+  tempalte<typename T>
+  class NodeDoubly{
+    public:
+        NodeDoubly* prev ;
+        T val ; 
+        NodeDoubly* next ; 
+        
+    NodeDoubly() : prev(NULL) ,val(NULL) , next(NULL) {};
+    
+ };
+ 
+ class Dlinkedlist{
+   public :
+   NodeDoubly *head ; 
+   NodeDoubly *tail  ;
+   
+   Dlinkedlist() : head(NULL), tail(NULL){} ;
+   
+   void insertendDLL(T data)
+   {
+       NodeDoubly* newnode = new NodeDoubly ;
+       newnode->val = data ; 
+       if(!head)
+       {
+           head = newnode ; 
+           tail = newnode ; 
+       }
+       else{
+           tail->next = newnode; 
+           newnode->prev = tail ; 
+           newnode->next = NULL ; 
+           tail = newnode ; 
+       }
+   }
+   void insertbeginDLL(T data){
+       NodeDoubly* newnode = new NodeDoubly ;
+       newnode->val = data ; 
+       if(!head)
+       {
+           head = newnode ; 
+           tail = newnode ; 
+       }
+       else{
+         newnode->next = head ; 
+         newnode->prev = tail ; 
+         head->prev = newnode ; 
+         head = newnode ; 
+       }
+   }
+   void travelDLL()
+   {
+       NodeDoubly* temp = head ; 
+       while(temp)
+       {
+           cout<<temp->val<< " " ;
+           temp = temp->next ; 
+        }
+       
+    }
+    void deleteEndDLL()
+    {
+        NodeDoubly *temp = head ; 
+        if(!head){
+            head = NULL  ;
+            tail = NULL ; 
+        }
+        else{
+            while(temp->next != tail )
+            {
+                temp = temp->next ; 
+            }
+            temp->next = NULL ;
+            tail->prev = NULL; 
+            tail = temp ; 
+        }
+    }
+   
+ };
+ 
+ template<typename T>
+ class CircularLL{
+      public:
+      Nodesingle* head ; 
+      Nodesingle* tail ;
+      
+      CircularLL() : head(NULL) , tail(NULL){} ;
+      
+      void InsertCLL(T data)
+      {
+          Nodesingle* newnode = new Nodesingle ;
+          newnode->val = data ; 
+          if(!head)
+          {
+           head = newnode ;
+           newnode->next = newnode ; 
+           head = newnode ; 
+           tail = newnode ; 
+         }
+       else{
+         newnode->next = head ; 
+         tail->next = newnode ; 
+         tail = newnode ; 
+       }
+      }
+      
+      void  Deleteatend()
+      {
+         Nodesingle *temp = head ; 
+        if(!head){
+            head = NULL ;
+            tail = NULL ; 
+        }
+        else{
+            while(temp->next != tail )
+            {
+                temp = temp->next ; 
+            }
+            temp->next = NULL ;
+            tail = temp ; 
+        } 
+      }
+        
+        void printCLL()
+        {
+            Nodesingle* temp = head ;
+            while(temp)
+            {
+                cout << temp->val << " " ;
+                temp = temp->next ;
+                if(temp == head)
+                break ; 
+            }
+        }
+    };
